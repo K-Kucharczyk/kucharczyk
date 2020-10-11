faf7d0276050a24058653590af3aa14025efaef9
K-Kucharczyk <56219412+K-Kucharczyk@users.noreply.github.com>
2020-10-11 20:41:23 +0200

    Add files via upload

--- java.js
+++ java.js
@@ -1,5 +1,20 @@
-
-function f()
+function b()
 {
-document.bgColor="black";
+p1.style.backgroundColor = "black";
+}
+function r()
+{
+p1.style.backgroundColor = "red";
+}
+function jb()
+{
+p1.style.backgroundColor = "#8ab7ff";
+}
+function sz()
+{
+p1.style.backgroundColor = "#545454";
+}
+function pin()
+{
+p1.style.backgroundColor = "pink";
 }
